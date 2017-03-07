5ba5127f4ce704b1e5a7907ac8e94554d287fc84
Hudolf Hess <hudolf@gmail.com>
2017-03-07 12:55:26 -0300

    Creating an action and the bindAction

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 
 class BookList extends React.Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li key={book.title} onClick={() => this.props.selectBook(book)} className="list-group-item">{book.title}</li>
       )
     })
   }
@@ -21,8 +23,13 @@ class BookList extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    books: state.books
+    books: state.books,
+    onSelectBook: selectBook
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
