c73cb00372cf727e85e43b7a529ed5af52443364
Hudolf Hess <hudolf@gmail.com>
2017-03-06 23:46:04 -0300

    Creating BookList container and connecting with reducer

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookList from '../containers/book_list'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+      </div>
+    )
+  }
+}
